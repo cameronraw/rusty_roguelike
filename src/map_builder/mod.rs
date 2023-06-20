d7ec31727909d0a1938eeab8d8df52040657f15a
Cameron Raw <cameron.raw89@gmail.com>
2023-06-20 12:42:03 +0200

    Introduces MapArchitect trait

---
+++ src/map_builder/mod.rs
@@ -0,0 +1,48 @@
+mod empty;
+
+use crate::prelude::*;
+
+use self::empty::EmptyArchitect;
+
+trait MapArchitect {
+    fn new(&mut self, rng: &mut RandomNumberGenerator) -> MapBuilder;
+}
+
+pub struct MapBuilder {
+    pub map: Map,
+    pub rooms: Vec<Rect>,
+    pub monster_spawns: Vec<Point>,
+    pub player_start: Point,
+    pub amulet_start: Point,
+}
+
+impl MapBuilder {
+    pub fn new(rng: &mut RandomNumberGenerator) -> Self {
+        let mut architect = EmptyArchitect {};
+        architect.new(rng)
+    }
+    fn fill(&mut self, tile: TileType) {
+        self.map.tiles.iter_mut().for_each(|t| *t = tile);
+    }
+
+    fn find_most_distant(&self) -> Point {
+        let dijkstra_map = DijkstraMap::new(
+            SCREEN_WIDTH,
+            SCREEN_HEIGHT,
+            &vec![self.map.point2d_to_index(self.player_start)],
+            &self.map,
+            1024.0
+        );
+
+        const UNREACHABLE: &f32 = &f32::MAX;
+
+        self.map.index_to_point2d(
+            dijkstra_map.map
+                .iter()
+                .enumerate()
+                .filter(|(_, dist)| *dist < UNREACHABLE)
+                .max_by(|a,b| a.1.partial_cmp(b.1).unwrap())
+                .unwrap().0
+        )
+    }
+}
